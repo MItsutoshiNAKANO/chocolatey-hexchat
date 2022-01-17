d60ba46ae5301f8465f6afb1d6a35ad428aed137
Mitsutoshi Nakano <bkbin005@rinku.zaq.ne.jp>
2022-01-18 08:49:20 +0900

    upgrade hexchat 2.14.2 -> 2.16.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,24 @@
-$packageName = 'hexchat'
-$installerType = 'EXE'
-$url = 'https://dl.hexchat.net/hexchat/HexChat%202.14.2%20x86.exe'
-$url64 = 'https://dl.hexchat.net/hexchat/HexChat%202.14.2%20x64.exe'
-$silentArgs = '/SILENT' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT"
-$validExitCodes = @(0)
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.hexchat.net/hexchat/HexChat%202.16.0%20x86.exe'
+$url64      = 'https://dl.hexchat.net/hexchat/HexChat%202.16.0%20x64.exe'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'hexchat*'
+
+  checksum      = 'f3f0ccd7d14369b48d103db95a91729e97afa4f051353ef2a640cd6d9f6f37ac'
+  checksumType  = 'sha256'
+  checksum64    = '5e4b7c0d599a4062b651855c46a1c030633da531041818b3fbed6403f73bea2e'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/SILENT"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
